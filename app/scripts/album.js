3cc86a02324a31d5b9e76323bc194052898dafda
hollyrohani <hollyrohani@gmail.com>
2015-05-22 13:46:18 -0700

    Added functionality to player controls

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -118,5 +118,47 @@
    // Wait until the HTML is fully processed.
    $(document).ready(function() { 
      changeAlbumView(albumPicasso);
+     setupSeekBars();
    });
  }
+ 
+  var updateSeekPercentage = function($seekBar, event) {
+   var barWidth = $seekBar.width();
+   var offsetX = event.pageX - $seekBar.offset().left;
+    
+   var offsetXPercent = (offsetX  / barWidth) * 100;
+   offsetXPercent = Math.max(0, offsetXPercent);
+   offsetXPercent = Math.min(100, offsetXPercent);
+ 
+   var percentageString = offsetXPercent + '%';
+   $seekBar.find('.fill').width(percentageString);
+   $seekBar.find('.thumb').css({left: percentageString});
+ }
+ 
+  var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+ 
+
+  $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+    
+    $seekBar.addClass('no-animate');
+ 
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+ 
+    $(document).bind('mouseup.thumb', function(){
+	    $seekBar.removeClass('no-animate');
+	    
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+ 
+  });
+
+}; 
