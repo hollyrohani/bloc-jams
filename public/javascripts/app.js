dc938fc1a533e1124798f4779c890b6b55fd30d3
hollyrohani <hollyrohani@gmail.com>
2015-06-15 14:53:37 -0700

    Changed slider to update (music location) data on change.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -278,12 +278,11 @@ require.register("scripts/album", function(exports, require, module) {
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
-     ]
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }     ]
  };
  
  
@@ -445,6 +444,14 @@ require.register("scripts/album", function(exports, require, module) {
        this.setSong(this.currentAlbum, song);
      },
      
+       seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         currentSoundFile.setTime(time);
+       }
+     },
+     
      setSong: function(album, song) {
 	  if (currentSoundFile) {
       currentSoundFile.stop();
@@ -473,19 +480,46 @@ require.register("scripts/album", function(exports, require, module) {
      return offsetXPercent;
    }
 	 
+	 var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+ 
 	 
    return {
      templateUrl: '/templates/directives/slider.html',
      replace: true,
       restrict: 'E',
-      scope: {},
+      scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
 	   scope.value = 0;
-       scope.max = 200;
+       scope.max = 100;
       var $seekBar = $(element);
       
+        attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+ 
+      
       var percentString = function () {
-         var percent = Number(scope.value) / Number(scope.max) * 100;
+         var value = scope.value || 0;
+		 var max = scope.max || 100;
+		 percent = value / max * 100;
          return percent + "%";
        }
  
@@ -500,7 +534,15 @@ require.register("scripts/album", function(exports, require, module) {
        scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+         notifyCallback(scope.value);
        }
+            
+       var notifyCallback = function(newValue) {
+       	if(typeof scope.onChange === 'function') {
+	   		scope.onChange({value: newValue});
+	   		notifyCallback(scope.value);
+         }
+       };
            
     }
   };
