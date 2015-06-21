1ce1bec57ae8ff192b7d435487a90bef967a65d9
hollyrohani <hollyrohani@gmail.com>
2015-06-21 15:39:54 -0700

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -141,6 +141,14 @@
    blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    	$scope.songPlayer = SongPlayer;
    
+   $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+ 
     SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -159,6 +167,7 @@
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -199,6 +208,13 @@
 	 onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    
+        setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
 	 
     setSong: function(album, song) {
       if (currentSoundFile) {
@@ -211,6 +227,8 @@
         formats: [ "mp3" ],
         preload: true
       });
+      
+      currentSoundFile.setVolume(this.volume);
 
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -402,6 +402,14 @@ require.register("scripts/album", function(exports, require, module) {
    blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    	$scope.songPlayer = SongPlayer;
    
+   $scope.volumeClass = function() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+ 
     SongPlayer.onTimeUpdate(function(event, time){
      $scope.$apply(function(){
        $scope.playTime = time;
@@ -420,6 +428,7 @@ require.register("scripts/album", function(exports, require, module) {
      currentSong: null,
      currentAlbum: null,
      playing: false,
+     volume: 90,
  
      play: function() {
        this.playing = true;
@@ -460,6 +469,13 @@ require.register("scripts/album", function(exports, require, module) {
 	 onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    
+        setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
 	 
     setSong: function(album, song) {
       if (currentSoundFile) {
@@ -472,6 +488,8 @@ require.register("scripts/album", function(exports, require, module) {
         formats: [ "mp3" ],
         preload: true
       });
+      
+      currentSoundFile.setVolume(this.volume);
 
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
